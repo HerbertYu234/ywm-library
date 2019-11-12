15950ce0917bdb23dd3e6bb46066e0276a7f67a4
yuhongbo <1359320936@qq.com>
2019-11-12 22:51:05 +0800

    init

---
+++ shared/src/main/java/ywm/library/shared/SharedAutoConfiguration.java
@@ -0,0 +1,10 @@
+package ywm.library.shared;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Herbert Yu on 2019-11-12 22:35
+ */
+@Configuration
+public class SharedAutoConfiguration {
+}
